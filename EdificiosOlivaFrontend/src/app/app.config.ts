1be829643e1e806ac717d183bc7a44a374cf2efd
Jairo Matías <jairo_1829@hotmail.com>
2026-07-26 17:20:12 -0400

    feat(frontend): register global API error handling

--- EdificiosOlivaFrontend/src/app/app.config.ts
+++ EdificiosOlivaFrontend/src/app/app.config.ts
@@ -1,29 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
+import {
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 import { routes } from './app.routes';
-
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-
-import { provideAuth, getAuth } from '@angular/fire/auth';
-
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-
-import { provideStorage, getStorage } from '@angular/fire/storage';
-
 import { firebaseConfig } from './core/config/firebase.config';
+import { apiErrorInterceptor } from './core/interceptors/api-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-
     provideRouter(routes),
-
-    provideHttpClient(),
-
+    provideHttpClient(
+      withInterceptors([apiErrorInterceptor]),
+    ),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
