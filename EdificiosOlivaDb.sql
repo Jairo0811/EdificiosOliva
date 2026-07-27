0eb2587c1078b30628aaa575785aa964f4e49e14
Jairo Matías <Jairo0811@users.noreply.github.com>
2026-07-26 21:31:31 -0400

    cambios

--- EdificiosOlivaDb.sql
+++ EdificiosOlivaDb.sql
@@ -1,19 +1,13 @@
 USE EdificiosOlivaDb;
 GO
 
-UPDATE Apartments
-SET Status = 1
-WHERE Status = 0;
-
-SELECT Id, Name, Status
-FROM Apartments;
-
-
-
-
-USE EdificiosOlivaDb;
-GO
-
-SELECT COUNT(*)
+SELECT
+    Id,
+    Name,
+    PricePerNight,
+    Status,
+    CreatedAtUtc,
+    UpdatedAtUtc,
+    IsDeleted
 FROM Apartments
-WHERE IsDeleted = 0;
+ORDER BY CreatedAtUtc DESC;
