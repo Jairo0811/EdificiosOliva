7cbe8f384919cebefbb6396b7d51ebf918ba6fb7
Jairo Matías <jairo_1829@hotmail.com>
2026-09-19 21:22:21 -0400

    test: mock home visual data sources

--- EdificiosOlivaFrontend/src/app/pages/home/home.spec.ts
+++ EdificiosOlivaFrontend/src/app/pages/home/home.spec.ts
@@ -1,7 +1,9 @@
-import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
+import { of } from 'rxjs';
 
+import { Apartments } from '../../core/services/apartments';
+import { GalleryImages } from '../../core/services/gallery-images';
 import { Home } from './home';
 
 describe('Home', () => {
@@ -11,11 +13,35 @@ describe('Home', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Home],
-      providers: [provideRouter([]), provideHttpClient()],
+      providers: [
+        provideRouter([]),
+        {
+          provide: Apartments,
+          useValue: {
+            getAvailableApartments: () => of([]),
+          },
+        },
+        {
+          provide: GalleryImages,
+          useValue: {
+            getAll: () =>
+              of({
+                items: [],
+                page: 1,
+                pageSize: 100,
+                totalItems: 0,
+                totalPages: 0,
+                hasPreviousPage: false,
+                hasNextPage: false,
+              }),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
+    fixture.detectChanges();
     await fixture.whenStable();
   });
 
